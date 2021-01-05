c7d1a732d042fa87a08d5aabc5c1dad6e482efea
DeadSecurityTraceur <76254042+DedSec-Traceur@users.noreply.github.com>
2021-01-04 22:52:50 -0800

    Add files via upload

---
+++ HackLive.py
@@ -0,0 +1,86 @@
+# usr/bin/python2
+# Grade By DaedSecurityTracuer / Syber 041
+# import 
+import os 
+import sys 
+import time 
+from art import *
+from colorama import *
+init()
+
+os.system('clear' or 'cls')
+
+# =================================
+# menu
+print (" GRADE BY DEDSEC and AMIR")
+print ("    TEAM is Syber 041")
+print ("|_________________________|")
+print ("|_________________________|")
+tprint('MENU')
+name = raw_input("@{Enter The Name = ")
+os.system('clear' or 'cls')
+print ("   Chose Options Theme </> ")
+print 
+print (Fore.WHITE+" [ 1 ] "+Fore.LIGHTGREEN_EX+"Theme is GREEN")
+print (Fore.WHITE+" [ 2 ] "+Fore.RED+"Theme is RED")
+print (Fore.WHITE+" [ 3 ] "+Fore.BLUE+"Theme is BLUE")
+print (Fore.WHITE+" [ 4 ] "+Fore.MAGENTA+"Theme is MEGANTA")
+print 
+theme = input("Enter The THEME = ")
+print (Fore.WHITE+" [ 1 ] "+"HACK LIVE")
+print (Fore.WHITE+" [ 2 ] "+"HACK LIVE 2")
+print 
+select = input("Enter The Select = ")
+if theme == 1:
+        print (Fore.LIGHTGREEN_EX)
+elif theme == 2:
+        print (Fore.RED)
+elif theme == 3:
+        print (Fore.BLUE)
+elif theme == 4:
+        print (Fore.MAGENTA)
+time.sleep(2)
+os.system('clear' or 'cls')
+tprint(name+" HACKER ")
+if select == 1:
+        while True:
+                print ("PLASE CHECK HACK/HACKERS IS DONT WEAR !")
+                time.sleep(1)
+                print ("CHECK BY HACKERS WIRLES WIFI !")
+                time.sleep(1)
+                print ("[MASAGE] Anonymous MASAGE = YOU IS VERY HACKER NOT IS NOOB !")
+                time.sleep(1)
+                print ("IS CHECK ONE VIRUSE  IS IM KILLER ROBOT !")
+                time.sleep(1)
+                print ("KILL THE VIRUS %10  KILL THE VIRUS %25 !")
+                time.sleep(1)
+                print ("KILL THE %50 KILL THE VIRUSE %100 !")
+                time.sleep(1)
+                print ("START HACK AND DDOS ATACK BY NASA !")
+                time.sleep(1)
+                print ("HACK IS DARK WEB SITE AND GOOGLE IS VERY ESEY !")
+                time.sleep(1)
+                print("1 SMS NASA = YOU IS VERY VERY BAD NO IS HACK IS NASA PLASE !")
+                time.sleep(1)
+elif select == 2:
+        while True:
+                print ("HACK BY SERVER WEB CAMERA AND NETHANTER 10")
+                time.sleep(1)
+                print ("HACK BY SERVER WEB CAMERA AND NETHANTER 20")
+                time.sleep(1)
+                print ("HACK BY SERVER WEB CAMERA AND NETHANTER 30")
+                time.sleep(1)
+                print ("HACK BY SERVER WEB CAMERA AND NETHANTER 40")
+                time.sleep(1)
+                print ("HACK BY SERVER WEB CAMERA AND NETHANTER 50")
+                time.sleep(1)
+                print ("HACK BY SERVER WEB CAMERA AND NETHANTER 60")
+                time.sleep(1)
+                print ("HACK BY SERVER WEB CAMERA AND NETHANTER 70")
+                time.sleep(1)
+                print ("HACK BY SERVER WEB CAMERA AND NETHANTER 80")
+                time.sleep(1)
+                print ("HACK BY SERVER WEB CAMERA AND NETHANTER 90")
+                time.sleep(1)
+                print ("HACK BY SERVER WEB CAMERA AND NETHANTER 100")
+                time.sleep(1)
---
+++ Termuxinstall.py
@@ -0,0 +1,6 @@
+import os
+
+os.system('pkg install python2')
+os.system('pkg install pip')
+os.system('pip install art')
+os.system('pip install colorama')
